190c1afb70dce47f08df25f6cc1c2a425d5f901a
nafisnihal <nafisnihal12@gmail.com>
2022-10-12 22:15:46 +0600

    toast added

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Options = ({allOption}) => {
-
-    return (
-        <div className='text-black'>
-            <p className='bg-white my-3 p-2 w-4/5 mx-auto rounded-md'>{allOption}</p>
-        </div>
-    );
+const Options = ({ allOption, correct }) => {
+  return (
+    <div className="text-black">
+      <p className="bg-white my-3 p-2 w-4/5 mx-auto rounded-md cursor-pointer hover:bg-yellow-400">
+        <button onClick={() => correct(allOption)}>{allOption}</button>
+      </p>
+    </div>
+  );
 };
 
 export default Options;
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Options from '../Options/Options';
+  import { ToastContainer, toast } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
 
 const Question = ({ allQuestion }) => {
   console.log(allQuestion);
-  const { question, options } = allQuestion;
+  const { correctAnswer, question, options } = allQuestion;
+
+  const correct = (option) =>{
+    if(option === correctAnswer){
+        toast.success("Correct Answer");
+    }else{
+       toast.error('Wrong Answer');
+    }
+  }
+
   return (
-    <div className='bg-slate-900 my-6 text-center p-4 rounded-lg w-2/3 mx-auto'>
-        <p className='mt-3 mb-6 py-5 mx-2  bg-yellow-400 rounded-md '>{question}</p>
-        {
-            options.map(option => <Options
-            key={option}
-            allOption={option}
-            ></Options>)
-        }
+    <div className="bg-slate-900 my-6 text-center p-4 rounded-lg w-2/3 mx-auto">
+      <p className="mt-3 mb-6 py-5 mx-2  bg-yellow-400 rounded-md ">
+        {question}
+      </p>
+      {options.map((option) => (
+        <Options key={option} allOption={option} correct={correct}></Options>
+      ))}
+      <ToastContainer position="top-center" autoClose={500} />
     </div>
   );
 };
